578cbbdffd863aa93465e9b6330684406127cb4d
Rob Cardy <rob.cardy@shopify.com>
2019-09-10 00:01:01 -0400

    Add a TODO

--- todos.js
+++ todos.js
@@ -1,7 +1,6 @@
 /*
     This is a list of TODOs
 
-    - Have different builds as we level up. Ideally, each room control level will have its own builds
-        - Builds should be more dynamic. Body type should be specified in a config file and pulled into the creation method instead of having a new method for each build
+    - Write new builds for level 3
 
 */
