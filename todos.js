e200f9c7d85ac3e0ad4470d7be86ebdb9774942a
Rob Cardy <rob.cardy@shopify.com>
2019-09-12 18:10:02 -0400

    Add another todo

--- todos.js
+++ todos.js
@@ -3,4 +3,6 @@
 
     - If a creep is carrying something that isn't energy, have them store it
     - Write something that knows how many spots each source has
+        - Perhaps polling how many creeps are using each source and adjusting that if a source has run out would be good? I could make more efficient use of available resources tha way.
+    - Start using Ramparts
 */
